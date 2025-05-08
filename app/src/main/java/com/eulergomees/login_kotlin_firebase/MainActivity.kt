c221eb1687036dc2bb59e9a2e3c642eb945bf0e1
Euler <eulergomesrocha@gmail.com>
2025-05-08 11:01:49 -0300

    Adds basic user authentication screen

--- app/src/main/java/com/eulergomees/login_kotlin_firebase/MainActivity.kt
+++ app/src/main/java/com/eulergomees/login_kotlin_firebase/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.eulergomees.login_kotlin_firebase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Password
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,44 +27,82 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.eulergomees.login_kotlin_firebase.ui.theme.LoginkotlinfirebaseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LoginkotlinfirebaseTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AuthScreen()
+                    AuthScreen(
+                        onEnterClick = {
+                            Log.i("MainActivity", "User: $it")
+                        }
+                    )
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AuthScreen() {
-    Column {
+fun AuthScreen(onEnterClick: (User) -> Unit = {}) {
+    Column(modifier = Modifier.padding(
+        top = 16.dp,
+        bottom = 16.dp
+    )) {
         var username by remember {
             mutableStateOf("")
         }
         var password by remember {
             mutableStateOf("")
         }
-        Text(text = "Username")
-        TextField(value = username, onValueChange = { newValue -> username = newValue })
-        Text(text = "Password")
+
+        TextField(
+            value = username,
+            onValueChange = { newValue ->
+                username = newValue
+            },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            label = { Text("Username") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "User")
+            }
+        )
         TextField(
             value = password,
             onValueChange = { newValue -> password = newValue },
-            visualTransformation = PasswordVisualTransformation()
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            label = { Text("Password") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Password, contentDescription = "Password")
+            },
+            visualTransformation = PasswordVisualTransformation(),
         )
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = {
+                onEnterClick(
+                    User(
+                        username,
+                        password
+                    )
+                )
+            },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+        ) {
             Text(text = "Login")
         }
     }
@@ -70,7 +116,7 @@ private fun AuthScreenPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            AuthScreen()
+            AuthScreen(onEnterClick = {})
         }
     }
 }
---
+++ app/src/main/java/com/eulergomees/login_kotlin_firebase/User.kt
@@ -0,0 +1,7 @@
+package com.eulergomees.login_kotlin_firebase
+
+data class User(
+    val username: String,
+    val password: String
+) {
+}
