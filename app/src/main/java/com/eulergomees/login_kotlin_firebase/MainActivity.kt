552b893998042469596a6a2251a9ab669cb5d56c
Euler <eulergomesrocha@gmail.com>
2025-05-08 09:33:30 -0300

    initializes project with basic auth UI

--- app/src/main/java/com/eulergomees/login_kotlin_firebase/MainActivity.kt
+++ app/src/main/java/com/eulergomees/login_kotlin_firebase/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.eulergomees.login_kotlin_firebase.ui.theme.LoginkotlinfirebaseTheme
 
@@ -19,11 +27,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginkotlinfirebaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthScreen()
                 }
             }
         }
@@ -31,17 +39,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AuthScreen() {
+    Column {
+        var username by remember {
+            mutableStateOf("")
+        }
+        var password by remember {
+            mutableStateOf("")
+        }
+        Text(text = "Username")
+        TextField(value = username, onValueChange = { newValue -> username = newValue })
+        Text(text = "Password")
+        TextField(
+            value = password,
+            onValueChange = { newValue -> password = newValue },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Login")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun AuthScreenPreview() {
     LoginkotlinfirebaseTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AuthScreen()
+        }
     }
 }
